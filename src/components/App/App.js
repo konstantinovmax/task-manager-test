a1c2ce4ebfd4cd6a46644748001f7eaf01396016
Maxim Konstantinov <konstantinovmax@gmail.com>
2022-09-30 20:29:43 +0300

    feat: Create a basic project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.content}></div>
+    </div>
+  );
+};
+
+export default App;
